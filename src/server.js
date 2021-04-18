d3e669193f9e168cb713279ed5f9fd5072558f69
Daniel Magalhães <magalhaes.ds@gmail.com>
2021-04-18 17:42:16 -0300

    Criando servidor e template engine

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
 
-server.get('/', (request, response) => {
-    return response.send('Oi!!!')
-})
+server.set("view engine", "ejs")
 
-server.listen(3000, () => console.log('rodando'))
+server.use(express.static("public"))
+
+server.use(routes)
+
+server.listen(3000, () => console.log("rodando"))
